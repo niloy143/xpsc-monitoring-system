4104fe24ffdcd3b8f11257e88fbba239683e8526
niloy143 <niloymahmudapu@gmail.com>
2023-10-29 23:32:50 +0600

    @vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { inject } from '@vercel/analytics';
+ 
 const queryClient = new QueryClient();
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
